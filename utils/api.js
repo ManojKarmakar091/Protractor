cbcca98c1f5b0ec3d0d2c450e67275fc9e2a2776
Manoj Karmkar <manoj.karmkar@synapse.com>
2020-01-28 12:16:11 +0530

    First commit2

--- utils/api.js
+++ utils/api.js
@@ -30,7 +30,80 @@ console.log('Failed to execute GET REQ'+ url & ' Found Error as ',  error )
 
 }
 
+this.postRequest= async function(getAPIUrl,datapayload){
 
+    try{
+    
+        const headers= {
+    
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
+        const response = await axios({
+    
+    method: 'post',
+    url: getAPIUrl,
+    data: datapayload,
+    headers:headers,
+    
+     });
+     return await response;
+    
+    }catch(error){
+    
+    console.log('Failed to execute POST REQ'+ url & ' Found Error as ',  error )
+    
+    }
+    }
+
+
+
+    this.calculateAPIResponse = async function(res){
+
+    const apiResTotalName= [];
+    if(res.status== 200){
+     const totalResponseLength= await res.data.data.length;
+     console.log('totalResponseLength', totalResponseLength)
+    for(let i=0; i<totalResponseLength; i++ ){
+
+       const outputName= res.data.data[i].name
+       apiResTotalName.push(outputName);
+    }
+return apiResTotalName;
+
+ }
+
+}
+
+    this.putRequest= async function(getAPIUrl, payload){
+
+    try{
+    
+        const headers= {
+    
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        };
+        const response = await axios({
+    
+    method: 'put', 
+    url: getAPIUrl,
+    body: payload,
+    headers:headers,
+    
+     });
+     return await response;
+    
+    }catch(error){
+    
+    console.log('Failed to execute PUT REQ'+ url & ' Found Error as ',  error )
+    
+    }
+    
+    
+    
+    }
+    
 }
 
 module.exports= new apiHelper();
--- utils/sampleTestCase.spec.js
+++ utils/sampleTestCase.spec.js
@@ -1,5 +1,6 @@
 
 var api= require('./api');
+var apiPayLoadJson= require('./apiPayLoad.json')
 
 
 describe('API Testing', ()=>{
@@ -22,7 +23,61 @@ await expect(res.status).toEqual(200)
 
 })
 
+it('API:POST CALL', async()=>{
+const url = "https://reqres.in/api/users"
+
+const payload = 
+    {
+        "name": "manoj",
+        "job": "qa"
+    }
+
+    const res= await api.postRequest(url, payload);
+    console.log('data = ', await res.data); // Print all data
+    console.log('status:  ', await res.status); // Print ststus code
+    await expect(res.status).toEqual(201)
+    
+    
+    })
 
 
+    it('API:PAYLOAD VALIDATION', async()=>{
+        const apiPayLoadSize= apiPayLoadJson.length;
+        console.log('size', apiPayLoadSize);
+        for(let i=0; i<apiPayLoadSize; i++){
+
+const UserName= apiPayLoadJson[i].name;
+console.log('UN', UserName);
+
+        }      
+    })
+
+    it('API:GET CALL 2', async()=>{
+
+        const res= await api.getRequest('https://reqres.in/api/unknown');
+        console.log('data = ', await res.data); // Print all data
+        console.log('status:  ', await res.status); // Print ststus code
+      
+       const name= await api.calculateAPIResponse(res);
+       console.log('name output =', name)
+       //await expect(name).toEqual('cerulean')
+      
+        
+        })
+
+        it('API:PUT CALL', async()=>{
+            const url = "https://reqres.in/api/users/2";
+            const payload= {
+                "name": "morpheus",
+                "job": "zion resident"
+            }
+
+            const res= await api.putRequest(url, payload);
+            console.log('PUT data = ', await res.data); // Print all data
+            console.log('PUT status:  ', await res.status); // Print ststus code
+          
+            
+            })
+    
 
 })
